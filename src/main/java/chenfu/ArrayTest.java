6fe5e09a9a0d7aacfe8f7696e53da2febd664a44
chenfu <16639050617@163.com>
2019-09-28 22:10:06 +0800

    OK

--- src/main/java/chenfu/ArrayTest.java
+++ src/main/java/chenfu/ArrayTest.java
@@ -1,9 +1,27 @@
 package chenfu;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: romantic_ke@163.com
  * @Description:
  * @Date: 2019/6/18 12:16
  */
 public class ArrayTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List proxyList = (List) Proxy.newProxyInstance(list.getClass().getClassLoader(), list.getClass().getInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                return method.invoke(list, args);
+            }
+        });
+        proxyList.add(123);
+        System.out.println(proxyList);
+
+    }
 }
